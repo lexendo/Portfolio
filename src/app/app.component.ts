d1d88a267bce9c8e19c95cbe2de4dd1deddfd660
Ondrej Babinský <babinskyondrej@gmail.com>
2025-01-29 04:41:30 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ThemeToggleComponent} from '../shared/theme-toggle/theme-toggle.component';
-import {ThemeService} from '../services/theme.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ThemeToggleComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   standalone: true
 })
 export class AppComponent {
-  constructor(private themeService: ThemeService) {}
 }
